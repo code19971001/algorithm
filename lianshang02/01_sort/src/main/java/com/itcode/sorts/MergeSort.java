3410c8483305b9c9bd7172546479684d205110bf
演员 <1753853993@qq.com>
2024-07-30 22:15:15 +0800

    liangshang02 add insertion sort and merge sort

--- lianshang02/01_sort/src/main/java/com/itcode/sorts/MergeSort.java
+++ lianshang02/01_sort/src/main/java/com/itcode/sorts/MergeSort.java
@@ -3,11 +3,11 @@ package com.itcode.sorts;
 @SuppressWarnings("unchecked")
 public class MergeSort<E extends Comparable<E>> extends Sort<E> {
 
-    private E[] backupArr;
+    private E[] leftArr;
 
     @Override
     protected void sort() {
-        backupArr = (E[]) new Comparable[arr.length >> 1];
+        leftArr = (E[]) new Comparable[arr.length >> 1];
         sort(0, arr.length);
     }
 
@@ -29,7 +29,7 @@ public class MergeSort<E extends Comparable<E>> extends Sort<E> {
      */
     private void merge(int begin, int mid, int end) {
         //备份数组
-        System.arraycopy(arr, begin, backupArr, 0, mid - begin);
+        System.arraycopy(arr, begin, leftArr, 0, mid - begin);
         int index = begin;
         for (int i = begin, j = mid; i <= mid && j <= end; i++, j++) {
             if (i == mid) {
@@ -38,11 +38,11 @@ public class MergeSort<E extends Comparable<E>> extends Sort<E> {
             }
             if (j == end) {
                 //一旦右边先结束. 需要将左边的剩余数据复制到右边
-                System.arraycopy(backupArr, i - begin, arr, index, mid - i);
+                System.arraycopy(leftArr, i - begin, arr, index, mid - i);
                 break;
             }
-            if (cmpElement(backupArr[i - begin], arr[j]) <= 0) {
-                arr[index++] = backupArr[i - begin];
+            if (cmpElement(leftArr[i - begin], arr[j]) <= 0) {
+                arr[index++] = leftArr[i - begin];
                 j--;
 
             } else {
@@ -58,21 +58,25 @@ public class MergeSort<E extends Comparable<E>> extends Sort<E> {
      * 为了更好的进行merge操作，最好将其中1组序列备份出来，推荐将左边的数组进行备份。
      */
     private void merge2(int begin, int mid, int end) {
+        //左边数组永远从0开始，数组长度为mid - begin
         int li = 0, le = mid - begin;
+        //右边数组
         int ri = mid, re = end;
+        //合并数组的索引
         int ai = begin;
         //备份数组
         for (int i = li; i < le; i++) {
-            backupArr[i] = arr[begin + i];
+            leftArr[i] = arr[begin + i];
         }
         //必定有一个到了终点：
         // 左边的数组到了重点，无需任何操作
         // 右边数组到了重点，需要复制左边数组的剩余内容到arr中去。
         while (li < le) {
-            if (ri < re && cmpElement(arr[ri], backupArr[li]) < 0) {
+            if (ri < re && cmpElement(arr[ri], leftArr[li]) < 0) {
+                //左右数组都没完成遍历，且左值大于右值
                 arr[ai++] = arr[ri++];
             } else {
-                arr[ai++] = backupArr[li++];
+                arr[ai++] = leftArr[li++];
             }
         }
     }
--- lianshang02/01_sort/src/main/java/com/itcode/sorts/QuickSort.java
+++ lianshang02/01_sort/src/main/java/com/itcode/sorts/QuickSort.java
@@ -1,8 +1,70 @@
 package com.itcode.sorts;
 
+/**
+ * 快速排序: 逐渐将每一个元素都转化为轴点, 一旦所有的元素都转换为轴点, 那就已经排好顺序.
+ * 最好时间复杂度: O(nlogn), 轴点左右元素数量比较均匀的情况下
+ * 最坏时间复杂度: O(n^2)
+ * <p>
+ * 平均时间复杂度推到:
+ * T(n) = 2 * T(n/2) + O(n)
+ * T(1) = O(1)
+ * T(n)/n = T(n/2)/ (n/2) + O(n)/n
+ * <p>
+ * 令S(n) = T(n)/n  S(1) = O(1)
+ * <p>
+ * S(n) = S(n/2) + O(1) = S(n/4) + O(2) = S(n/(2^3)) + O(3) = S(n/(2^k)) + O(k) = S(1) + O(logn)
+ * T(n) = nS(n) = O(nlogn)
+ */
 public class QuickSort<E extends Comparable<E>> extends Sort<E> {
     @Override
     protected void sort() {
+        sort(0, arr.length);
+    }
 
+    private void sort(int begin, int end) {
+        if (end - begin < 2) {
+            return;
+        }
+        //确定轴点元素的位置
+        int pivotIndex = getPivotIndex(begin, end); // O(n)
+        sort(begin, pivotIndex); //T(n/2)
+        sort(pivotIndex + 1, end); // T(n/2)
+    }
+
+    /**
+     * 将轴点元素放入最终位置
+     */
+    private int getPivotIndex(int begin, int end) {
+        E pivotVal = arr[begin];
+        //默认反向
+        boolean reverse = true;
+        //指向最后一个元素
+        end--;
+        while (begin < end) {
+            if (reverse) {
+                if (cmpElement(pivotVal, arr[end]) < 0) {
+                    end--;
+                } else {
+                    //覆盖
+                    arr[begin++] = arr[end];
+                    //下一轮正向扫
+                    reverse = false;
+                }
+            } else {
+                if (cmpElement(pivotVal, arr[begin]) > 0) {
+                    begin++;
+                } else {
+                    //覆盖
+                    arr[end--] = arr[begin];
+                    //下一轮反向扫
+                    reverse = true;
+
+                }
+            }
+
+        }
+        //将轴点元素放到合适的位置
+        arr[begin] = pivotVal;
+        return begin;
     }
 }
